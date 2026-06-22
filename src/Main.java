36e626c1eae79a6c1ea1d13a6b3b174d1d5e83b5
Anjali Pal <palanjali659@gmail.com>
2026-06-22 22:31:52 +0530

    Added studyRecord class and completed Study Tracker

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import data.model.StudyRecord;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -21,9 +23,32 @@ public class Main {
         switch (choice) {
 
     case 1:
-        System.out.println("Study Tracker selected");
-        break;
 
+    System.out.print("Enter Subject Name: ");
+    String subject = sc.nextLine();
+
+    System.out.print("Enter Hours Studied: ");
+    int hours = sc.nextInt();
+
+    System.out.print("Enter Today's Goal:");
+    sc.nextLine();
+    String goal = sc.nextLine();
+
+StudyRecord record = new StudyRecord();
+
+    record.subjectName = subject;
+    record.hoursStudied = hours;
+    record.todayGoal = goal;
+
+    System.out.println("\nStudy Record Saved Successfully!");
+    System.out.println("Subject : " + record.subjectName);
+    System.out.println("Hours   : " + record.hoursStudied);
+    System.out.println("Goal    : " + record.todayGoal);
+
+    System.out.print("Enter Today's Goal:");
+    
+   
+    break;
     case 2:
         System.out.println("Coding Tracker selected");
         break;
---
+++ src/data/model/StudyRecord.java
@@ -0,0 +1,7 @@
+package data.model;
+
+public class StudyRecord {
+   public String subjectName;
+   public  int hoursStudied;
+    public String todayGoal;
+}
